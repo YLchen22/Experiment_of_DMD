5a32d46050649013e39e5ac90cbbbe42ecdb4ae3
cyrandom <yuanlvchen@gmail.com>
2025-03-04 08:38:54 +0800

    visualization

---
+++ latest/vals_distance.m
@@ -0,0 +1,4 @@
+function err = vals_distance(vals1, vals2)
+    err = norm(vals1 - vals2, 'fro');
+end
+
---
+++ latest/vecs_distance.m
@@ -0,0 +1,6 @@
+function err = vecs_distance(vecs1, vecs2)
+    prod = vecs1' * vecs2;
+    diag_err = diag(abs(prod) - eye(size(prod)));
+    err = norm(diag_err, 'fro');
+end
+
